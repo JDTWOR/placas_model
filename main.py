e5d41061469cc9201e186d265503aea2ea81f2e2
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-09-05 02:57:34 -0500

    Ignorar carpeta venv

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="rf_iGsNbgEKxYaly45mJLSkx5WAl6T2")
+rf = Roboflow(api_key="stpB56ARIMQO2pA5pclc")
 project = rf.workspace("roboflow-universe-projects").project("license-plate-recognition-rxg4e")
-model = project.version(1).model
+model = project.version(11).model
 
 # Inferencia en una imagen
-prediction = model.predict("placa.jpg").json()
+prediction = model.predict("placa.png").json()
 print(prediction)
+model.predict("placa.png").save("resultado.png")
